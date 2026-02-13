754c9cb2e6db7cc435dd93de2c3f7bd75eb00cae
sangwon-jung <tkddnjs11122@naver.com>
2026-02-13 19:17:04 +0900

    클라우드DB로 교체

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# 1. 데이터베이스 접속 주소
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/aidb"
+# 로컬 데이터베이스 접속 주소
+#SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/aidb"
+
+# .env 파일에서 환경 변수를 로드합니다.
+load_dotenv()
+
+# 1. 환경 변수에서 DB 주소를 가져옵니다. 
+# 만약 .env에 없다면 기존 로컬 주소를 기본값으로 사용합니다.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # 2. SQLALCHEMY 엔진 생성
 # 엔진은 DB에 실제 연결을 관리하는 핵심 객체입니다.
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"connect_timeout": 10} # 연결 대기 시간 설정)
+)
 
 # 3. 세션 생성기
 # 세션은 DB와 대화하는 '단위'입니다. (데이터를 넣거나 뺄 때 이 세션을 사용합니다.)
